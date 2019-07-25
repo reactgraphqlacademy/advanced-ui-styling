cb383c4d857c6a37e802bbe5e012ccf548dab873
alexlbr <alex.lobera@gmail.com>
2019-07-25 20:54:01 +0100

    updates MyBoxIsNotADiv.js

--- src/pages/MyBoxIsNotADiv.js
+++ src/pages/MyBoxIsNotADiv.js
@@ -3,7 +3,7 @@ import Box from "../components/Box";
 
 const Page = () => (
   <div>
-    <h1>Flexbox</h1>
+    <h1>My Box is not a div</h1>
     <p>
       You can change the component that is rendered by using the <code>as</code>{" "}
       from styled components
