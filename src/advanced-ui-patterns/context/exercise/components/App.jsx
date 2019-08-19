46d142079ecc6c410e3c3fc7ce227feb93de7b46
alexlbr <alex.lobera@gmail.com>
2019-08-19 16:46:12 +0100

    updates Context exercise

--- src/advanced-ui-patterns/context/exercise/components/App.jsx
+++ src/advanced-ui-patterns/context/exercise/components/App.jsx
@@ -1,15 +1,11 @@
 import React from "react";
-import { ThemeContext } from "./ThemeProvider";
+// import { ThemeContext } from "./ThemeProvider";
 import Button from "./Button";
 import Hero from "./Hero";
 
 const App = () => (
   <Hero>
-    <ThemeContext.Consumer>
-      {({ handleThemeToggle }) => (
-        <Button onClick={handleThemeToggle}>Theme Switcher</Button>
-      )}
-    </ThemeContext.Consumer>
+    <Button onClick={() => {}}>Theme Switcher</Button>
   </Hero>
 );
 
