656d1b718d7cd9660511e330c3ef6b83c98dbb61
alexlbr <alex.lobera@gmail.com>
2019-12-16 17:01:02 +0000

    add Link component

--- src/design-system/components/navigation/Link.js
+++ src/design-system/components/navigation/Link.js
@@ -3,6 +3,8 @@ import styled from "styled-components";
 import { Link as RRLink } from "react-router-dom";
 import Box from "../layout/Box";
 
+const Link = styled(Box)``;
+
 const GenericLink = ({ to, ...rest }) =>
   to && to.match(/^(https:\/\/*|http:\/\/*|mailto:*)/) ? (
     <a href={to} {...rest} />
@@ -10,4 +12,9 @@ const GenericLink = ({ to, ...rest }) =>
     <RRLink to={to} {...rest} />
   );
 
-export default GenericLink;
+Link.defaultProps = {
+  as: GenericLink,
+  color: "pink"
+};
+
+export default Link;
