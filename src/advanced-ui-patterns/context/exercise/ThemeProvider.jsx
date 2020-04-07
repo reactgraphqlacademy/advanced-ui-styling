295df9c394954ca6d90de96861e2690a36005223
alexlbr <alex.lobera@gmail.com>
2020-04-07 22:24:39 +0100

    update comment for the context exercise

--- src/advanced-ui-patterns/context/exercise/ThemeProvider.jsx
+++ src/advanced-ui-patterns/context/exercise/ThemeProvider.jsx
@@ -2,18 +2,18 @@ import React from "react";
 import themes from "./theme";
 import { ThemeProvider as StyledProvider } from "styled-components";
 
-//TODO 1, create your `ThemeContext` (use React.CreateContext());
+//Task 1, create your `ThemeContext` (use React.CreateContext());
 
 export const ThemeProvider = ({ children }) => {
   const [theme, setTheme] = React.useState("dark");
   const toggleTheme = () => setTheme(theme === "dark" ? "light" : "dark");
 
-  // TODO 2, wrap the StyledProvider with your ThemeContext.Provider and share the toggleTheme function
+  // Task 2, wrap the StyledProvider with your ThemeContext.Provider and share the toggleTheme function
   return <StyledProvider theme={themes[theme]}>{children}</StyledProvider>;
 };
 
-// TODO 3, implement the useTheme and share the context like the example useModal
+// Task 3, implement the useTheme and share the context like the example useModal
 export const useTheme = () => {};
-// TODO 4, use the useTheme custom hook in the ./ToggleThemeButton.jsx component
+// Task 4, use the useTheme custom hook in the ./ToggleThemeButton.jsx component
 
 export default ThemeProvider;
